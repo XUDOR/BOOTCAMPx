84cc7d34cb284ba40292e2d13dacfae3c281104a
Roderick Shoolbraid <rdxenv@gmail.com>
2023-12-08 17:27:05 -0800

    Parameterized Query SQL injection update

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
 
+// Set up your database connection information
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -12,6 +13,7 @@ const cohortName = process.argv[2];
 const limit = process.argv[3] || 5; // Default to 5 if no limit is provided
 
 // Parameterized query to prevent SQL injection
+// Using placeholders ($1, $2) for dynamic values
 const queryString = `
   SELECT students.id, students.name, cohorts.name AS cohort_name
   FROM students
@@ -20,8 +22,10 @@ const queryString = `
   LIMIT $2;
 `;
 
+// Values to be used in the query - corresponds to placeholders in queryString
 const queryValues = [`%${cohortName}%`, limit];
 
+// Executing the query with parameterized values
 pool.query(queryString, queryValues)
   .then(res => {
     res.rows.forEach(user => {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
 
+// Set up your database connection information
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -7,8 +8,12 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = process.argv[2] || 'JUL02'; // default value if not provided
+// Capture the cohort name from command-line arguments
+// Default to 'JUL02' if no argument is provided
+const cohortName = process.argv[2] || 'JUL02';
 
+// Parameterized query to prevent SQL injection
+// Using a placeholder ($1) for the dynamic cohort name value
 const queryString = `
   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM teachers
@@ -19,11 +24,15 @@ const queryString = `
   ORDER BY teacher;
 `;
 
+// Values to be used in the query - corresponds to the placeholder in queryString
 const queryValues = [cohortName];
 
+// Executing the query with parameterized values
 pool.query(queryString, queryValues)
   .then(res => {
+    // Iterate over each row in the response
     res.rows.forEach(row => {
+      // Log the cohort and teacher name
       console.log(`${row.cohort}: ${row.teacher}`);
     });
   })
